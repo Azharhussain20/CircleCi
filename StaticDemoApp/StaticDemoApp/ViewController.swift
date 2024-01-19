4a325a1287f4eb906e7c06aabbc787812458b1d2
Azharhussain <azhar@peerbits.com>
2024-01-19 10:48:57 +0530

    Initial commit

---
+++ StaticDemoApp/StaticDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticDemoApp
+//
+//  Created by Azhar - M1 on 18/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
